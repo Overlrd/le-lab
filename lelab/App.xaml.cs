cca76afb75b89faeee38b56bd2e9acf016d3c447
Aug8stin KPADONOU <premieraugust28@gmail.com>
2022-09-06 14:24:24 +0200

    Add files via upload

---
+++ lelab/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using lelab.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace lelab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CountPage ();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ lelab/Views/CountPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lelab.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CountPage : ContentPage
+    {
+        public CountPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CountButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Bouton cliqué");
+        }
+    }
+}
